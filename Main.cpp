dbdd3c779fe028c36689a09b5a8f4e91e3445e6e
Zubabubal <145017356+Zubabubal@users.noreply.github.com>
2024-10-26 13:53:22 +0200

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,35 @@
 #include "Cube.h"
 
+void MainMenu()
+{
+	cout << "\n\tMenu:\n";
+	cout << "Enter 1 to create normal cube\n";
+	cout << "Enter 2 to create random cube\n";
+	cout << "Enter 3 to create cube from file data\n";
+	cout << "Enter 4 to check the cube\n";
+	cout << "Enter 5 to see the cube\n";
+	cout << "Enter 6 to move faces\n";
+	cout << "Enter 7 to quit\n\n";
+}
+
+void MoveMenu()
+{
+	cout << "Enter 1 to move left column" << endl;
+	cout << "Enter 2 to move right column" << endl;
+	cout << "Enter 3 to move high row" << endl;
+	cout << "Enter 4 to move down row" << endl;
+	cout << "Enter 5 to move front face" << endl;
+	cout << "Enter 6 to move back face" << endl;
+}
+
+int RandomNumber(int a, int b)
+{
+	random_device random_device;
+	mt19937 generator(random_device());
+	uniform_int_distribution<> distribution(a, b);
+	int number = distribution(generator);
+	return number;
+}
 
 int main()
 {
